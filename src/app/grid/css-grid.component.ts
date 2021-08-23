9f5fb1a47b02015e7f5362e055a87f13f090354a
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2021-08-23 17:51:06 -0500

    update interface

--- src/app/grid/css-grid.component.ts
+++ src/app/grid/css-grid.component.ts
@@ -19,10 +19,16 @@ import {
   // CdkDrop,
 } from '@angular/cdk/drag-drop';
 
-import { keys, values } from 'lodash';
+export interface SensorMapKey {
+  key: string;
+  label: string;
+}
 
-import { Mapping, MapItem } from '../types';
-import { getAvailableKeys, toMapItem } from '../utils';
+export interface SensorMapItem {
+  id: string;
+  name: string;
+  key: string;
+}
 
 @Component({
   selector: 'app-css-grid',
@@ -30,91 +36,86 @@ import { getAvailableKeys, toMapItem } from '../utils';
   styleUrls: ['./css-grid.component.scss']
 })
 export class CssGridComponent implements OnInit {
-  sensorMapKeys: MapItem[] = [];
-  sensorMapList: MapItem[][] = [];
-  availableSensors: MapItem[] = [];
-  sensorDropConnections: CdkDropList<MapItem>[][] = [];
-  availableConnections: CdkDropList<MapItem>[] = [];
+  sensorMapKeys: SensorMapKey[] = [
+    { key: "Door", label: "Door" },
+    { key: "Handle", label: "Handle" },
+    { key: "Lock", label: "Lock" }
+  ];
+  availableSensors: SensorMapItem[] = [];
+  sensorMapList: SensorMapItem[][];
+
+  sensorDropConnections: CdkDropList<SensorMapItem>[][] = [];
+  availableConnections: CdkDropList<SensorMapItem>[] = [];
   mappingPlaceholder: boolean[] = [];
 
   isDragStat = false;
-  sensorMapping: Mapping = {
-    Door: null,
-    Handle: 'Contact Handle 1',
-    Lock: null
-  };
 
-  @ViewChildren('sensorMapDropList') sensorMapDropList: QueryList<CdkDropList<MapItem>>;
-  @ViewChild('availableDropList', { static: true }) availableDropList: CdkDropList<MapItem>;
+  @ViewChildren('sensorMapDropList') sensorMapDropList: QueryList<CdkDropList<SensorMapItem>>;
+  @ViewChild('availableDropList', { static: true }) availableDropList: CdkDropList<SensorMapItem>;
 
   ngOnInit() {
     this.initProps();
     this.emitMapping();
-
   }
 
   connectDropsites() {
+    const mapIds = this.sensorMapList.map((v) => v.length > 0 ? v[0].id : null);
+    this.availableSensors = this.availableSensors.filter((data) => {
+      return mapIds.filter((id) => id && id === data.id).length === 0;
+    });
+
     const naArr = this.sensorMapDropList.toArray();
     this.availableConnections = naArr.filter((_na, idx) => this.sensorMapList[idx].length === 0);
     this.sensorDropConnections = naArr.map((_cd, idx) => {
-      const conns = naArr.filter((_na, naIdx) => naIdx !== idx
-        && this.sensorMapList[naIdx].length === 0);
+      const conns = naArr.filter((_na, naIdx) => naIdx !== idx && this.sensorMapList[naIdx].length === 0);
       conns.push(this.availableDropList);
       return conns;
     });
 
     console.log(`sensorMapKeys=`, this.sensorMapKeys)
     console.log(`sensorMapList=`, this.sensorMapList)
+    // console.log( ' newMapping xxx= ', this.newMapping)
     console.log(`available=`, this.availableSensors)
 
-    console.log(`sensorDropConnections=`,this.sensorDropConnections)
+    console.log(`sensorDropConnections=`, this.sensorDropConnections)
     console.log(`availableConnections=`, this.availableConnections)
     console.log(`mappingPlaceholder=`, this.mappingPlaceholder)
   }
 
   emitMapping() {
-    const mapping: Mapping = {};
+    // const mapping: Mapping = {};
     this.sensorMapList.forEach((v, idx) => {
-      mapping[this.sensorMapKeys[idx].key] = (v.length > 0) ? v[0].key : null;
+      // mapping[this.sensorMapKeys[idx].key] = (v.length > 0) ? v[0].key : null;
     });
     // this.mapping.emit(mapping);
   }
 
   initProps() {
-    this.sensorMapKeys = keys(this.sensorMapping).map(toMapItem);
-    this.availableSensors = getAvailableKeys(this.sensorMapping).map(toMapItem);
-    this.availableSensors.push({
-      key: "E", displayName: "E"
-    },{
-      key: "F", displayName: "F"
-    });
-    const vals = values(this.sensorMapping);
-    this.sensorMapList = vals.map(v => v ? [toMapItem(v)] : []);
-    this.mappingPlaceholder = vals.map(v => !v);
+    this.sensorMapList = [
+      [],
+      [{ id: '12', key: 'Handle', name: 'Contact Handle2' }],
+      []
+    ];
+    this.availableSensors = [
+      { id: '12', key: "Door", name: "Contact Handle2" },
+      { id: '13', key: "Handle", name: "Handle" },
+      { id: '14', key: "Lock", name: "Lock" },
+      { id: '15', key: "E", name: "E" },
+      { id: '16', key: "F", name: "F" }
+    ];
+    this.mappingPlaceholder = this.sensorMapList.map((v) => v.length === 0);
   }
 
-  /**
-   * Calls initializer method(s) that use the @ViewChild and @ViewChildren properties.
-   */
+
   ngAfterViewInit() {
     // Change template data bindings after current event loop tick as per
     // https://angular.io/guide/component-interaction#parent-calls-an-viewchild
     setTimeout(() => this.connectDropsites());
   }
 
-  /**
-   * Performs all the backing changes that must happen when a draggable item is dropped into
-   * a dropsite.
-   *
-   * Specifically: Rearrange the items when an item is dropped within the same dropsite, else:
-   * 1. Move the item to the new dropsite's backing array,
-   * 2. Update the dropsites' sensorDropConnections to each other, and
-   * 3. Emit the new item map via the mapping @Output.
-   *
-   * @param event Event from cdk-drop's "dropped" event.
-   */
-  cdkDragDrop(event: CdkDragDrop<MapItem[]>, idx: number) {
-    console.log( ' dropped=', event)
+
+  cdkDragDrop(event: CdkDragDrop<SensorMapItem[]>, idx: number) {
+    console.log(' dropped=', event)
     this.isDragStat = false;
     if (event.previousContainer === event.container) {
       moveItemInArray(
@@ -131,17 +132,11 @@ export class CssGridComponent implements OnInit {
       );
       this.mappingPlaceholder[idx] = false;
       this.connectDropsites();
-      this.emitMapping();
+      // this.emitMapping();
     }
   }
 
-  /**
-   * Sets whether the "Drag and drop here to assign" message is displayed in one of the
-   * dropsites in the "New Assignments" column.
-   *
-   * @param idx Index of the dropsite in the "New Assignments" column.
-   * @param show Whether the placeholder should be shown in that dropsite.
-   */
+
   setMapPlaceholder(idx: number, show: boolean) {
 
     // this.mappingPlaceholder[idx] = show;
@@ -149,8 +144,8 @@ export class CssGridComponent implements OnInit {
   }
 
   cdkDragMoved() {
-   console.log('drag move')
-   this.isDragStat = true;
+    console.log('drag move')
+    this.isDragStat = true;
     // this.mappingPlaceholder[0] = false;
   }
 
